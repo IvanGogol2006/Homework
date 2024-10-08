37fb4d949971d8c14df62507587c987373e2faca
IvanGogol2006 <v9224846156.2@gmail.com>
2024-10-08 22:33:35 +0500

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,6 @@
+kol_vo_DZ   = 12
+kol_vo_chasov = 1.5
+kurs = 'python'
+time = kol_vo_chasov/kol_vo_DZ
+print('Курс:' ,kurs, 'Всего заданий:',kol_vo_DZ, 'Всего потрачено времени:',kol_vo_chasov, 'Время на одно задание:' ,time)
+
