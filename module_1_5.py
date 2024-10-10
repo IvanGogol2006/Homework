270899397f05577a7d135ddcf8ee29b9a21b006e
IvanGogol2006 <v9224846156.2@gmail.com>
2024-10-10 20:48:44 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 1, 2, 3, 'a' , 'b' , 'c'
+print(immutable_var)
+#immutable_var [2] = 4 - нельзя изменять данные типа tuple
+mutable_list = [1, 'apple' ,2, 'True' , 3, 4]
+mutable_list[0] = 6
+print(mutable_list)
