e6a68a63ec6e2ccf7a85a8bfb43dffd972b86ebc
IvanGogol2006 <v9224846156.2@gmail.com>
2024-10-29 21:54:38 +0500

    Add files via upload

---
+++ module_2.hard.py
@@ -0,0 +1,16 @@
+import random
+a = random.randint (1, 20)
+print(a)
+result = []
+
+for i in range(1, a+1):
+    for k in range(1,20):
+        if a % (k+i) == 0 and i != 0 and i <= k:
+            result.append(i)
+            result.append(k)
+print("".join(map(str, result)))
+
+
+
+
+
