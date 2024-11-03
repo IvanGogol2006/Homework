ffe4940c0a986b4058d09a82b5ae1019c28a6ef4
IvanGogol2006 <v9224846156.2@gmail.com>
2024-11-03 16:58:49 +0500

    Update module_2.hard.py

--- module_2.hard.py
+++ module_2.hard.py
@@ -1,11 +1,11 @@
 import random
-a = random.randint (1, 20)
+a = random.randint (3, 20)
 print(a)
 result = []
 
 for i in range(1, a+1):
     for k in range(1,20):
-        if a % (k+i) == 0 and i != 0 and i <= k:
+        if a % (k+i) == 0 and i != 0 and i < k:
             result.append(i)
             result.append(k)
 print("".join(map(str, result)))
